8f5930b17ca53f545ba2d539c29e9235cd1b6240
MrFlava <thatelitemaili33t@gmail.com>
2024-09-13 11:25:43 +0300

    structs , reciever funcs and pointers

--- structs.go
+++ structs.go
@@ -4,6 +4,10 @@ import "fmt"
 
 func main(){
 	mybill := newBill("my bill")
-
-	fmt.Println(mybill)
+	mybill.addItem("onion soup", 4.50)
+	mybill.addItem("veg", 4.50)
+	mybill.addItem("perepichka", 4.50)
+	mybill.addItem("coffee", 4.50)
+	mybill.updateTip(10)
+	fmt.Println(mybill.format())
 }

ddf0172e21eaea89b64c4bf155c4b32608875ac1
MrFlava <thatelitemaili33t@gmail.com>
2024-09-17 16:24:10 +0300

    switches and parsing floats

--- userinput.go
+++ userinput.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"bufio"
 	"strings"
+	"strconv"
 )
 
 func getInput(prompt string, r *bufio.Reader) (string, error){
@@ -30,7 +31,40 @@ func promptOptions(b bill){
 	reader := bufio.NewReader(os.Stdin)
 
 	opt, _ := getInput("Choose option (a- add item, s - save bill, t - add tip): ", reader)
-	fmt.Println(opt)
+
+	switch opt {
+	case "a":
+		name, _ := getInput("Item name: ", reader)
+		price, _ := getInput("Item price: ", reader)
+
+		p, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			fmt.Println("The price must be a number")
+			promptOptions(b)
+		}
+		b.addItem(name, p)
+
+		fmt.Println("Item added -", name, price)
+		promptOptions(b)
+	case "t":
+		tip, _ := getInput("Enter tip amount ($): ", reader)
+
+		t, err := strconv.ParseFloat(tip, 64)
+		if err != nil {
+			fmt.Println("The tip must be a number")
+			promptOptions(b)
+		}
+		b.updateTip(t)
+
+		fmt.Println("tip added - ", tip)
+		promptOptions(b)
+	case "s":
+		fmt.Println("you chose to save the bill", b)
+
+	default:
+		fmt.Println("that was not a valid option...")
+		promptOptions(b)
+	}
 }
 
 func main(){

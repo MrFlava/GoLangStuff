d9920d7e0b325e31423a375546053b6e482ec042
MrFlava <thatelitemaili33t@gmail.com>
2024-09-21 17:16:14 +0300

    channels, but there's some wierd stuff there

---
+++ chanells.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+	"sync"
+)
+
+func DoWork() int {
+	time.Sleep(time.Second)
+	return rand.Intn(1000)
+}
+
+func main(){
+
+	dataChan := make(chan int, 2)
+
+	go func() {
+		wg := sync.WaitGroup{}
+		for i := 0; i < 1000; i++ {
+			go func() {
+				defer wg.Done()
+				result := DoWork()
+				dataChan <- result
+			}()
+		}
+		wg.Wait()
+		close(dataChan)
+	}()
+
+	// go func(){
+	// 	dataChan <- 789
+	// }()
+
+	// dataChan <- 789
+	// dataChan <- 123
+
+	// n := <- dataChan
+	// fmt.Printf("n = %d\n", n)
+
+	// n = <- dataChan
+	// fmt.Printf("n = %d\n", n)
+
+	for n := range dataChan{
+		fmt.Printf("n = %d\n", n)
+	}
+}

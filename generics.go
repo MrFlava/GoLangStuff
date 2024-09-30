dbeb59c4f6a52f2dea6f398a5972cc166821ed75
MrFlava <thatelitemaili33t@gmail.com>
2024-09-30 16:50:04 +0300

    work with generics after problems

--- generics.go
+++ generics.go
@@ -2,12 +2,11 @@ package main
 
 import "fmt"
 
-func Add(a int, b int) int {
+func Add[T int | float64](a T, b T) T {
 	return a + b
 }
 
-func main(){
-	result := Add(1.1, 2)
+func main() {
+	result := Add(1.1, 21)
 	fmt.Printf("result: %v\n", result)
 }
-

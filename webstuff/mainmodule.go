54955d02043f7bdec0956d55bbda29a29d1a9d81
MrFlava <thatelitemaili33t@gmail.com>
2024-10-04 22:23:28 +0300

    Create the POST endpoint

--- webstuff/mainmodule.go
+++ webstuff/mainmodule.go
@@ -1,17 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type User struct {
+	Name string `json: "name"`
+}
+
+var userChahe = make(map[int]User)
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World")
 }
 
+func createUser(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if user.Name == "" {
+		http.Error(
+			w,
+			"name is reqired",
+			http.StatusBadRequest,
+		)
+		return
+	}
+
+	userChahe[len(userChahe)+1] = user
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handleRoot)
+
+	mux.HandleFunc("POST /users", createUser)
 
 	fmt.Println("Server is on 8080")
 	http.ListenAndServe(":8080", mux)

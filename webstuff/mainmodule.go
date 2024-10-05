c3d4a63d7b16f988169b4f9adb894f4d05311627
MrFlava <thatelitemaili33t@gmail.com>
2024-10-05 09:08:04 +0300

    Provide DELETE endpoint

--- webstuff/mainmodule.go
+++ webstuff/mainmodule.go
@@ -88,12 +88,42 @@ func getUser(
 	w.Write(j)
 }
 
+func deleteUser(
+	w http.ResponseWriter,
+	r *http.Request,
+) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.Error(
+			w,
+			err.Error(),
+			http.StatusBadRequest,
+		)
+		return
+	}
+
+	if _, ok := userChahe[id]; !ok {
+		http.Error(
+			w,
+			"User not found",
+			http.StatusNotFound,
+		)
+		return
+	}
+	chacheMutex.Lock()
+	delete(userChahe, id)
+	chacheMutex.Unlock()
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handleRoot)
 
 	mux.HandleFunc("POST /users", createUser)
 	mux.HandleFunc("GET /users/{id}", getUser)
+	mux.HandleFunc("DELETE /users/{id}", deleteUser)
 
 	fmt.Println("Server is on 8080")
 	http.ListenAndServe(":8080", mux)

4f6b67feaf3b6beee6a2f7ef89dde32d8e12c332
MrFlava <thatelitemaili33t@gmail.com>
2024-11-04 23:20:58 +0200

    Database inserting

--- Database usage/databases.go
+++ Database usage/databases.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	_ "github.com/lib/pq"
 )
 
+type Product struct {
+	Name      string
+	Price     float64
+	Avaliable bool
+}
+
 func main() {
-	connStr := "postgres://postgres:secret@localhost:5432/gopostgrestest?sslmode=disable"
+	connStr := "postgres://postgres:secret@localhost:5433/gopostgrestest?sslmode=disable"
 
 	db, err := sql.Open("postgres", connStr)
 
@@ -22,29 +29,46 @@ func main() {
 		log.Fatal(err)
 	}
 
-	createProductTable(db)
+	product := Product{"book", 15.55, true}
+	pk := insertProduct(db, product)
+
+	fmt.Printf("ID = %d\n", pk)
 
 }
 
-func createProductTable(db *sql.DB) {
-	/* Product Table
-	- ID
-	- Name
-	- Price
-	-Avaliable
-	- Date Created
-	*/
-	query := `CREATE TABLE IF NOT EXISTS product(
-		id SERIAL PRIMARY KEY,
-		name VARCHAR(100) NOT NULL,
-		price NUMERIC(6,2) NOT NULL,
-		avaliable BOOLEAN,
-		created timestamp DEFAULT NOW()
-	)`
+// func createProductTable(db *sql.DB) {
+// 	/* Product Table
+// 	- ID
+// 	- Name
+// 	- Price
+// 	-Avaliable
+// 	- Date Created
+// 	*/
+// 	query := `CREATE TABLE IF NOT EXISTS product(
+// 		id SERIAL PRIMARY KEY,
+// 		name VARCHAR(100) NOT NULL,
+// 		price NUMERIC(6,2) NOT NULL,
+// 		avaliable BOOLEAN,
+// 		created timestamp DEFAULT NOW()
+// 	)`
 
-	_, err := db.Exec(query)
+// 	_, err := db.Exec(query)
+
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
+
+func insertProduct(db *sql.DB, product Product) int {
+	query := `INSERT INTO product(name, price, avaliable)
+		VALUES ($1, $2, $3) RETURNING id`
+
+	var pk int
+	err := db.QueryRow(query, product.Name, product.Price, product.Avaliable).Scan(&pk)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	return pk
 }
